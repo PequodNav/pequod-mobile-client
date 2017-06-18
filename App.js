ea76d2edcfde5921fa28b56752968a6d02b9e03a
Jonathan Kaufman <jondakauf@gmail.com>
2017-06-18 11:37:27 -0400

    fetch points, cache them, and render on the map

--- App.js
+++ App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View} from 'react-native';
 import { Constants, Location, Permissions, MapView } from 'expo';
 
-export default class App extends Component {
-  static route = {
-    navigationBar: {
-      visible: false,
-    },
-  }
+const BASE_URL = 'http://pequod.us-east-1.elasticbeanstalk.com';
+const EARTH_RADIUS = 3959;
 
+const PLATFORM_ERROR = 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!';
+const PERMISSION_ERROR = 'Permission to access location was denied';
+
+export default class App extends Component {
+  pointCache = {};
   state = {
     errorMessage: null,
     location: null,
+    points: [],
     region: {
       latitude: null,
       longitude: null,
@@ -20,22 +22,20 @@ export default class App extends Component {
     },
   };
 
+  /** When we mount, get location unless we're simulating android */
   componentWillMount() {
     if (Platform.OS === 'android' && !Constants.isDevice) {
-      this.setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-      });
+      this.setState({ errorMessage: PLATFORM_ERROR });
     } else {
       this._getLocationAsync();
     }
   }
 
+  /** set the location and region from the user's location */
   _getLocationAsync = async () => {
     const { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
-      this.setState({
-        errorMessage: 'Permission to access location was denied',
-      });
+      this.setState({ errorMessage: PERMISSION_ERROR });
     }
 
     const location = await Location.getCurrentPositionAsync({});
@@ -50,12 +50,39 @@ export default class App extends Component {
     });
   };
 
+  /** fetch points based on the current region */
+  _getPointsAsync = async () => {
+    const { latitude, longitude, latitudeDelta, longitudeDelta } = this.state.region;
+    const distance = 2 * Math.max(latitudeDelta, longitudeDelta) * EARTH_RADIUS;
+    const response = await fetch(`${BASE_URL}/points?lat=${latitude}&lng=${longitude}&distance=${distance}&limit=500`);
+    const points = await response.json();
+    this._addPoints(points || []);
+  }
+
+  /** add new points to our cache and then the map */
+  _addPoints(points = []) {
+    points.forEach(point => {
+      this.pointCache[point._id] = point;
+    });
+    this.setState({
+      points: Object.keys(this.pointCache).map(pointID => this.pointCache[pointID]),
+    });
+  }
+
+  /** control the region in state */
   onRegionChange(region) {
     this.setState({ region });
   }
 
+  /** when the region changed, fetch new points */
+  onRegionChangeComplete() {
+    if (this.state.location) {
+      this._getPointsAsync();
+    }
+  }
+
   render() {
-    if (!this.state.region.longitude && !this.state.region.latitude) {
+    if (!this.state.location) {
       return (
         <View style={styles.container}>
           <Text style={styles.paragraph}>{this.state.errorMessage || 'Loading...'}</Text>
@@ -68,7 +95,22 @@ export default class App extends Component {
         style={{ flex: 1 }}
         region={this.state.region}
         onRegionChange={region => this.onRegionChange(region)}
-      />
+        onRegionChangeComplete={() => this.onRegionChangeComplete()}
+        showsUserLocation={true}
+        showsTraffic={false}
+      >
+        {this.state.points.map(point => (
+          <MapView.Marker
+            key={point._id}
+            coordinate={{
+              latitude: point.loc.coordinates[1],
+              longitude: point.loc.coordinates[0]
+            }}
+            title={point.aidName}
+            description={`${point.summary} (${point.type}, ${point.lnmSource})`}
+          />
+        ))}
+      </MapView>
     );
   }
 }
